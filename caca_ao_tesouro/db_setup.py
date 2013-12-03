aec0908b70ff32d185a80c70044be219dac3989a
Bruna Xavier <bruninha221990@gmail.com>
2013-12-03 18:32:37 -0200

    Criando usuários do jeito CERTO. set_password MALDITO!

--- caca_ao_tesouro/db_setup.py
+++ caca_ao_tesouro/db_setup.py
@@ -2,7 +2,12 @@
 from django.contrib.auth.models import User
 User.objects.all().delete()
 
-User.objects.create(username="bruna", password="123mudar", first_name="Bruna", last_name="Xavier")
-User.objects.create(username="peri", password="123peri", first_name="Luiz", last_name="Pericolo")
+user = User.objects.create(username="bruna", first_name="Bruna", last_name="Xavier")
+user.set_password('123mudar')
+user.save()
+
+user = User.objects.create(username="peri", first_name="Luiz", last_name="Pericolo")
+user.set_password('123peri')
+user.save()
 
 print "{0} Users created!".format(User.objects.count())
