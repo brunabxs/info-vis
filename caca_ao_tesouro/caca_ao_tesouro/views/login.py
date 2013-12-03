f8b27edadbf26ac51abc86f6986c88167281b950
Bruna Xavier <bruninha221990@gmail.com>
2013-12-03 17:35:53 -0200

    Recuperando nome e senha da tela de login e validando o usuario.

--- caca_ao_tesouro/caca_ao_tesouro/views/login.py
+++ caca_ao_tesouro/caca_ao_tesouro/views/login.py
@@ -1,6 +1,23 @@
 # coding: utf-8
 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.views.decorators.http import require_POST, require_GET
+from django.core.context_processors import csrf
 
+from caca_ao_tesouro.models import User
+
+@require_POST
 def login(request):
-  return HttpResponse(u"Olá")
+  valid = User.validate_user(name=request.POST['name'], password=request.POST['password'])
+
+  return HttpResponse(valid)
+
+@require_GET
+def home(request):
+  c = {}
+  c.update(csrf(request))
+  return render_to_response("index.html", c)
+
+def new_user(request):
+  from caca_ao_tesouro.models.user import User
