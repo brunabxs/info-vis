9722223d7bfa19d275b1a7db4996fcb74b6e8c91
Luiz Pericolo <luizpericolo@gmail.com>
2013-12-04 23:54:04 -0200

    Listando as dicas para o usuário logado.

--- treasure_hunt/treasure_hunt/views/login.py
+++ treasure_hunt/treasure_hunt/views/login.py
@@ -18,16 +18,25 @@ def user_logout(request):
 @require_POST
 def login(request):
   from django.contrib.auth import authenticate, login
+  from treasure_hunt.models import ObjectTip
+
+  data = {}
 
   user = authenticate(username=request.POST['name'], password=request.POST['password'])
   if user is not None:
     login(request=request, user=user)
-    message = u'Logado!'
   else:
     user = create_new_user(username=request.POST['name'], password=request.POST['password'])
-    message = u'Criado novo usuário!'
 
-  return render_to_response("home.html", {'username': user.username})
+  data['username'] = user.username
+
+  data['tips'] = []
+
+  for tip in ObjectTip.objects.all():
+    data['tips'].append({'name': u"Dica {0}".format(tip.id), 'id': tip.id})
+
+
+  return render_to_response("home.html", data)
 
 @require_GET
 def home(request):
