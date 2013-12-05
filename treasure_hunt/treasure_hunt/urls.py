1324a518789390f43cb71db3e0d72c45dda6e6f2
Luiz Pericolo <luizpericolo@gmail.com>
2013-12-04 23:22:22 -0200

    Removendo comentaŕios.

--- treasure_hunt/treasure_hunt/urls.py
+++ treasure_hunt/treasure_hunt/urls.py
@@ -7,12 +7,6 @@ from django.conf.urls import patterns, include, url
 from treasure_hunt.views.login import login, home, user_logout
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'treasure_hunt.views.home', name='home'),
-    # url(r'^treasure_hunt/', include('treasure_hunt.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
