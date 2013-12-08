23a9744628f2d616d09ae59c633e6d8b2bf2f0df
Luiz Pericolo <luizpericolo@gmail.com>
2013-12-08 09:23:52 -0200

    Mudanças para mostrar o texto da dica na tela.

--- treasure_hunt/treasure_hunt/views/object_tip.py
+++ treasure_hunt/treasure_hunt/views/object_tip.py
@@ -2,8 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.http import require_POST, require_GET
 from django.contrib.auth.decorators import login_required
+from django.core.context_processors import csrf
+
+from treasure_hunt.models import ObjectTip
 
 @login_required
 @require_GET
 def show_tip(request, id):
-	return HttpResponse("Detalhe da Dica {0}!".format(id))
+    tip = ObjectTip.objects.get(id=id)
+    ctx = {
+        'tip_name': "Dica {0}".format(id),
+        'tip_description': tip.text
+    }
+    
+    ctx.update(csrf(request))
+
+    return render_to_response("scan_tip.html", ctx)
